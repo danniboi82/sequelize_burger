641cf0efe063c5e746b39aab0eeecb9a8b9ff728
Unknown <danoh82@gmail.com>
2017-12-10 18:42:53 -0800

    Database connects NO HTML !!
    도대체 뭘 잘못 집었을까??? 머리아퐈~

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,14 +1,15 @@
-
+var express = require("express");
+var router = express.Router();
 var db = require("../models");
 
 
-module.export = function(app) {
+module.export = function(router) {
 // get route -> index
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
   res.redirect("/burgers");
 });
 
-app.get("/burgers", function(req, res) {
+router.get("/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
   db.burger.findAll({}).then(function(dbburger) {
     res.json(dbburger);
@@ -18,7 +19,7 @@ app.get("/burgers", function(req, res) {
 });
 
 // post route -> back to index
-app.post("/burgers/create", function(req, res) {
+router.post("/burgers/create", function(req, res) {
   // takes the request object using it as input for buger.addBurger
   // burger.create(req.body.burger_name, function(result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
@@ -35,7 +36,7 @@ app.post("/burgers/create", function(req, res) {
 });
 
 // put route -> back to index
-app.put("/burgers/update", function(req, res) {
+router.put("/burgers/update", function(req, res) {
   // burger.update(req.body.burger_id, function(result) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
--- models/burger.js
+++ models/burger.js
@@ -1,34 +1,12 @@
-var orm = require("../config/orm.js");
-
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
-  }
-};
-
-module.exports = burger;
-
+var Sequelize = require("sequelize");
 
 module.exports = function(sequelize, DataTypes) {
   var burger = sequelize.define("burgers", {
-    burger_name: Datatypes.STRING,
-    eaten : Datatypes.BOOLEAN,
-  });
+    burger_name: DataTypes.STRING,
+    eaten : DataTypes.BOOLEAN,
+  
 }, {
   timestamp: true,
-}; return burger; 
+});
+  return burger; 
+};
